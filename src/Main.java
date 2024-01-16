9d5c4dd5b58538e76492c55fb82118b57ef0fb39
Lino Coria Mendoza <l.coria@northeastern.edu>
2024-01-16 11:10:11 -0800

    Checkstyle updates.

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,17 @@ import animals.Size;
  * We will also create a few tests to make sure that the Dog class is working as expected.
  */
 public class Main {
+
+  /**
+   * Main method.
+   * @param args command line arguments.
+   */
   public static void main(String[] args) {
     // Create a few Dog objects.
     Dog ichiro = new Dog("Ichiro", 12, 10.5, Size.SMALL, true);
     Dog lucky = new Dog("Lucky", 5, 13.455, Size.MEDIUM, false);
 
-// Print the toString() of the Dog objects.
+    // Print the toString() of the Dog objects.
     System.out.println("\nInformation about Ichiro:");
     System.out.println(ichiro);
     System.out.println("\nInformation about Lucky:");
